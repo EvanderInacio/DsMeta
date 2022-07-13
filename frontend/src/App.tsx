fa994466e9f01b25d844ddb272732c7ee30a0e4d
Evander Inacio <evander.20112@hotmail.com>
2022-07-12 21:58:23 -0300

    First component ⚒️

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
+import NotificationButton from "./components/NotificationButton"
 
 
 function App() {
   return (
-    <h1>Hello world</h1>
+    <>
+      <h1>Hello world</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
