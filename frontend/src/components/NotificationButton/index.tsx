b2f50276999ebad639152ffb72dd47455cff7e21
Evander Inacio <evander.20112@hotmail.com>
2022-07-16 16:24:52 -0300

    Toast 🎉

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,19 +1,20 @@
-import axios from 'axios';
+import axios from 'axios'
+import { toast } from 'react-toastify'
 import icon from '../../assets/img/notification-icon.svg'
-import { BASE_URL } from '../../utils/request';
+import { BASE_URL } from '../../utils/request'
 import './styles.css'
 
 type Props = {
-  saleId: number;
+  saleId: number
 }
 
-function handleClick(id : number) {
+function handleClick(id: number) {
   axios(`${BASE_URL}/sales/${id}/notification`).then(response => {
-    console.log("sucesso")
+    toast.info('SMS enviado com sucesso!!')
   })
 }
 
-function NotificationButton( {saleId} : Props ) {
+function NotificationButton({ saleId }: Props) {
   return (
     <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
       <img src={icon} alt="Notification" />
